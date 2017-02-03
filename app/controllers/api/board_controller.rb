e76b9c427241a3edbfc25d2b496ff42dc68d5b4b
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-02-03 12:01:50 +0000

    [board] Add a boards controller

--- app/controllers/api/board_controller.rb
+++ app/controllers/api/board_controller.rb
@@ -1,7 +1,21 @@
 module Api
   class BoardController < BaseController
     def index
-      render json: { ok: true }, status: :ok
+      @board = Board.first
+      render json: { led: @board.button }, status: :ok
     end
+
+    def create
+      @board = Board.find_or_create_by(uid: params[:mac])
+      @board.update board_params
+      render json: { ok: true }, status: :created
+    end
+    private
+
+    def board_params
+      params[:button] = params[:button] == "True"
+      params.permit(:button)
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :board, only: [:index]
+    resources :board, only: [:index, :create]
   end
 
   root to: 'home#index'
