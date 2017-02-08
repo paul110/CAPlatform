fe9eb0b878887057db3148cfc60424fd33f459e6
Paul Cristian <pp13003@my.bristol.ac.uk>
2017-02-08 10:55:14 +0000

    middleware solution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.middleware.use SketchActionCable
+  config.web_socket_server_url = "wss://caplatform.herokuapp.com/"
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
