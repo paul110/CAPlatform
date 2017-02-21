e9cdd5991bec816c8f00ee57540f9525957e7286
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-02-21 15:01:10 +0000

    [seeds] Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-Sketch.create! links: [{from: '1234', to: '5678', logic: 'toggle'}], boards: [{mac: '1234', centre: {x: 120, y: 120}, width: 40, height: 40}, {mac: '5678', centre: {x: 240, y: 120}, width: 40, height: 40}]
 Board.create! mac: "1234"
 Board.create! mac: "5678"
+Board.create! mac: "MacOS|Chrome|56.0.2924.87"
+
+Sketch.create! links: [{from: '1234', to: '5678', logic: 'toggle'}],
+  boards: [{mac: '1234', centre: {x: 120, y: 120}, width: 40, height: 40},
+    {mac: '5678', centre: {x: 240, y: 120}, width: 40, height: 40}]
+Sketch.create! links: [{to: "MacOS|Chrome|56.0.2924.87", from: "5678", logic: "link_opener"}],
+  boards: [{mac: "5678", width: 40, centre: {x: 224.5,y: 85},height: 40},
+    {mac: "MacOS|Chrome|56.0.2924.87",width: 40, centre: {x: 383.5,y: 84}, height: 40}]
