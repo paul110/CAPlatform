c60aaaab61cb07d8a0af5d26c97ea8dc462808d4
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-03 21:32:40 +0000

    [sketches] Rename the columns

--- db/migrate/20170303211454_add_user_id_to_sketches.rb
+++ db/migrate/20170303211454_add_user_id_to_sketches.rb
@@ -1,8 +1,8 @@
 class AddUserIdToSketches < ActiveRecord::Migration[5.0]
   def change
     add_column :sketches, :user_id, :integer
-    add_column :sketches, :purchased_from_id, :integer
+    add_column :sketches, :creator_id, :integer
     add_index :sketches, :user_id
-    add_index :sketches, :purchased_from_id
+    add_index :sketches, :creator_id
   end
 end
