ee5c8b8e1ee5e6ac57cda808290bc677cd5bdb31
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-03 21:12:53 +0000

    [users] Add users model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+
+class User < ApplicationRecord
+  has_many :sketches
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
