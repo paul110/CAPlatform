04daf90ec2491f79dc6a91266dc3e7dc74ac92e3
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-04 18:56:18 +0000

    [auth] Add a sessions controller to upsert the user

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email
+end
