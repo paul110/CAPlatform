3a9389c41b27d02f1a9a72e8c75b1f957120a906
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-05 11:06:20 +0000

    [rspec] Write real specs for the sessions controlller

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create, params: { name: "test name", email: "test_email@atrato.com", format: :json }
+  describe "POST #create" do
+    it "returns http created" do
+      post :create, params: { name: "test name", email: "test_email@atrato.com", format: :json }
 
       expect(response).to have_http_status :created
+    end
+
+    it "creates the user" do
+      expect {
+        post :create, params: { name: "test name", email: "test_email@atrato.com", format: :json }
+      }.to change {
+        User.count
+      }.by 1
+    end
+
+    it "sets the user email and name" do
+      post :create, params: { name: "test name", email: "test_email@atrato.com", format: :json }
+
+      last_user = User.last
+      expect(last_user.name).to eq "test name"
+      expect(last_user.email).to eq "test_email@atrato.com"
     end
   end
 
