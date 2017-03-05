a22bd45d85263cc69a38e8534cba208bf503da53
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-05 10:00:32 +0000

    [rspec] Improve specs and remove home controller

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Api::SessionsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      get :create, params: { name: "test name", email: "test_email@atrato.com", format: :json }
+
+      expect(response).to have_http_status :created
     end
   end
 
