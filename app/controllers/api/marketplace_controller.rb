c070c0c3377fa9b8d97ea5bbe2f48e89add499aa
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-08 17:10:04 +0000

    [marketplace] Add a purchase endpoint

--- app/controllers/api/marketplace_controller.rb
+++ app/controllers/api/marketplace_controller.rb
@@ -7,5 +7,30 @@ module Api
         format.json { render json: @sketches, each_serializer: SketchSerializer }
       end
     end
+
+    def purchase
+      @sketch = Sketch.create purchase_sketch_attributes.merge(new_params)
+      respond_to do |format|
+        format.json { render json: @sketch, status: :created }
+      end
+    end
+
+    private
+
+    def purchase_sketch_attributes
+      Sketch
+        .for_marketplace
+        .find(params.require(:sketch_id))
+        .attributes
+        .with_indifferent_access
+        .slice(:boards, :links, :name, :description)
+    end
+
+    def new_params
+      {
+        status: "closed",
+        user: User.find(params.require(:user_id))
+      }
+    end
   end
 end
