e4b887c978b085d796c112e5984180175847d54b
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-03-10 13:02:05 +0000

    [fix] Fix invalid board params

--- app/channels/sketch_channel.rb
+++ app/channels/sketch_channel.rb
@@ -2,13 +2,11 @@ class SketchChannel < ApplicationCable::Channel
   CHANNEL_NAME = "SketchChannel"
   def subscribed
     stream_from "sketch_channel#{params[:mac]}"
-    if board = Board.find_or_create_by(mac: params[:mac], type: params[:type])
-      if board.valid?
-        Log.connect(board.name, board.mac, CHANNEL_NAME)
-      else
-        Log.error "Invalid Board params(#{params.inspect}) #{board.mac}"
-      end
+    if board = find_board
+      Log.connect(board.name, board.mac, CHANNEL_NAME)
       board.update status: "online"
+    else
+      Log.error "Invalid Board params(#{params.inspect}) #{board.mac}"
     end
   end
 
@@ -23,6 +21,16 @@ class SketchChannel < ApplicationCable::Channel
     if board = Board.find_by(mac: params[:mac])
       Log.received "Received input from #{board.name}<#{board.mac}>"
       InputBroadcastJob.perform_now data, board
+    end
+  end
+
+  private
+
+  def find_board
+    if board = Board.find_by(mac: params[:mac])
+      board
+    else
+      Board.create(mac: params[:mac], type: params[:type])
     end
   end
 end
